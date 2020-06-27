48e6af50d878bd7d7a8302e4b07216394ffd5b3f
frazaoalex <alexfrazaodev@gmail.com>
2020-06-27 20:56:46 -0300

    Criar o projeto Spring

---
+++ src/main/java/com/edu/flf/pooii/Ds1LocadoraSpringApplication.java
@@ -0,0 +1,13 @@
+package com.edu.flf.pooii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds1LocadoraSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ds1LocadoraSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/flf/pooii/Ds1LocadoraSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.flf.pooii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds1LocadoraSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
