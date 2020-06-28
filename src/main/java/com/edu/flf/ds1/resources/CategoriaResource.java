9f664e17d49c238c284486f0b69fbba7541bc025
frazaoalex <alexfrazaodev@gmail.com>
2020-06-27 21:48:39 -0300

    Teste da primeira classe de dominio - Categoria

--- src/main/java/com/edu/flf/ds1/resources/CategoriaResource.java
+++ src/main/java/com/edu/flf/ds1/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.edu.flf.ds1.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edu.flf.ds1.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "Rest funfando...";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Comédia");
+		Categoria cat2 = new Categoria(2,"Ação");
+		Categoria cat3 = new Categoria(3,"Terror");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		categorias.add(cat3);
+		
+		return categorias;
 	}
 }
